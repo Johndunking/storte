bee5ef50880abee9a1d333b8ffd6396a5c63ed2d
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-11-16 23:15:20 -0800

    grammar corrections

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
          <div class="container text-center">
          <div className="row">
           <div className="col">
-            <h2 className="mt-4 services-text">Pricing & Garuntee</h2> {/* Add mt-4 class for top margin */}
+            <h2 className="mt-4 services-text">Pricing & Guaruntee</h2> {/* Add mt-4 class for top margin */}
           </div>
         </div>
             <div class="row">
@@ -23,7 +23,7 @@ function About() {
               <div class="col">
               <div class="card" style={{width: '28rem', backgroundColor: 'rgba(230, 226, 216, 0.5)', fontFamily:'agbalumo', textShadow: '-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white',}}>
                   <div class="card-body">
-                    <h5 class="card-title">All work is garunteed!</h5>
+                    <h5 class="card-title">All work is guarunteed!</h5>
                     <p class="card-text" style={{fontFamily:'Merriweather', fontSize:'12px'}}>Proudly serving any and all communities in Sacramento! <br /> Please give us a call at 1-888-888-8888 first for a full list of services offered.</p>
                   </div>
                   <img src="https://plus.unsplash.com/premium_photo-1661696478346-7a13f1731fbd?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGhhbmRzaGFrZSUyMGNvbnN0cnVjdGlvbnxlbnwwfHwwfHx8MA%3D%3D" class="card-img-bottom" alt="..."/>
